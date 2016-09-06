c7bd38251a22da6646d2bc286088d0c3ca7ea86f
Saul Romero <saul.romero@pearson.com>
2016-09-05 22:13:10 -0600

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
 
 
   root 'pages#home'
+
+#  match '/about', to: 'pages#about', via: 'get'
+#  get 'about', to: 'pages#about'
+  get 'about' => 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
